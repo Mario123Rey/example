85728ff37e5620a5ae9c1a2121555efeae7e9872
Mario <huertam9876@gmail.com>
2023-05-07 02:28:27 -0600

    primer cambio

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CursoController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
+Route::get('/',HomeController::class);
+
+
+/*Route::get('cursos',[CursoController::class,'index']);
+Route::get('cursos/create', [CursoController::class,'create']);
+Route::get('cursos/{curso}', [CursoController::class,'show']);*/
+
+Route::controller(CursoController::class)->group(function(){
+    Route::get('cursos','index');
+    Route::get('cursos/create', 'create');
+    Route::get('cursos/{curso}', 'show');
+});
